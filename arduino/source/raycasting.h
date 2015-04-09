512f22ccd54216b0ed0eb3370fa2c2419a29a59c
Buster Kim Mejborn <buster@mejborn.it>
2015-04-09 09:45:30 +0200

    Changed structure

--- arduino/source/raycasting.h
+++
@@ -1,6 +0,0 @@
-typedef struct { double x, y; } vec;
-typedef struct { int n; vec* v; } polygon_t, *polygon;
-
-#define BIN_V(op, xx, yy) vec v##op(vec a,vec b){vec c;c.x=xx;c.y=yy;return c;}
-#define BIN_S(op, r) double v##op(vec a, vec b){ return r; }
-
